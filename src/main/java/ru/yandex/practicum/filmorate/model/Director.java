f9fbaf48083a1774be8a7915f7c97613ad8cd8b2
Artem Mironyuk <sochemironiuk@yandex.ru>
2022-08-25 10:24:05 +0300

    feat:add director and directorcontroller

--- src/main/java/ru/yandex/practicum/filmorate/model/Director.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Director.java
@@ -1,4 +1,17 @@
 package ru.yandex.practicum.filmorate.model;
 
+import lombok.*;
+
+import javax.validation.constraints.NotBlank;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Director {
+
+    Integer id;
+    @NonNull
+    @NotBlank
+    String name;
 }
