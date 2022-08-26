0491bb712793f4d92e4610fe7eda119e2698a547
aseev_sa <asesergej@yandex.ru>
2022-08-26 16:28:44 +0300

    have to switch on another branch. OperationEnum, EventEnum and Feed.java were added

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/Feed.java
@@ -0,0 +1,18 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Feed {
+
+    private final long id;
+    private final long userId;
+    private final long eventId;
+    private final long entityId;
+    private final long timeTs;
+    private final EventEnum eventEnum;
+    private final OperationEnum operationEnum;
+
+}
