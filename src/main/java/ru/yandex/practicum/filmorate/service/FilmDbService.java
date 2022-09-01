185a5e802bedf944ae8f94b5c613e0d841a671c4
aseev_sa <asesergej@yandex.ru>
2022-09-01 11:34:53 +0300

    bugfix for develop-test

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmDbService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmDbService.java
@@ -106,9 +106,9 @@ public class FilmDbService implements FilmService, MpaService, GenreService{
                 filmStorage.addLike(filmId, userId);
                 feedDao.addFeed(userId, EventEnum.LIKE, OperationEnum.ADD, filmId);
                 log.info("Пользователь с ID {} поставил лайк фильму с ID {}", userId, filmId);
-            } else {
+/*            } else {
                 throw new ValidationException(String.format("Пользователь c ID %s уже " +
-                        "оценивал фильм с ID %s", userId, filmId));
+                        "оценивал фильм с ID %s", userId, filmId));*/
             }
         } else {
             throw new NotFoundException(String.format("Нет фильма с ID %s", filmId));
